a9a829609a6058c7a2fe79f8e29294f70016f6e7
KAJIWARA.Yutaka <kajiwara.yutaka@WIN-P07JHIL91I4.cm.local>
2015-06-02 02:27:34 +0900

    入力された値に応じてClass名の変更を実装

--- src/main/resources/static/js/controller/indexController.js
+++ src/main/resources/static/js/controller/indexController.js
@@ -2,5 +2,27 @@
     'use strict';
     angular.module('rcw').controller('indexController',function($scope){
 
+        /**
+         * 指定した文字列の長さと現在の文字列長に応じてクラス名を返す
+         * @param targetModel 対象文字列
+         * @param targetLength 最大文字列長
+         * @return targetModelの文字長に応じたクラス名
+         */
+        var lengthCheck = function(targetModel ,targetLength){
+            if(angular.isUndefined(targetModel)){
+                return 'text-muted';
+            }else{
+                var warningLength = parseInt(targetLength * 0.9);
+                if(targetModel.length > targetLength){
+                    return 'text-danger';
+                }else if(targetModel.length > warningLength){
+                    return 'text-warning';
+                }else{
+                    return 'text-muted';
+                }
+            }
+        };
+        $scope.lengthCheck = lengthCheck;
+
     });
 }());
